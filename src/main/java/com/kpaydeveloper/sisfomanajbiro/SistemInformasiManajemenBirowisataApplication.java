852200243071301b9f362d02f52c8853e17c2305
Deni Gunawan <artdengun@gmail.com>
2020-11-04 21:18:24 +0700

    sistem informasi manajemen birowista

---
+++ src/main/java/com/kpaydeveloper/sisfomanajbiro/SistemInformasiManajemenBirowisataApplication.java
@@ -0,0 +1,13 @@
+package com.kpaydeveloper.sisfomanajbiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemInformasiManajemenBirowisataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemInformasiManajemenBirowisataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpaydeveloper/sisfomanajbiro/SistemInformasiManajemenBirowisataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpaydeveloper.sisfomanajbiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemInformasiManajemenBirowisataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
